1259b6305372e9392d7146e6c737b06ad1932146
danvella02 <73609558+danvella02@users.noreply.github.com>
2021-01-14 00:26:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,54 +3,6 @@ const app = express ();
 
 app.use(express.json());
 
-let store = new Vue({
-data: { products: {} },
-// this function runs when creating the Vue instance
-created: function() {
-// replace the URL to your Heroku app and route
-fetch('http://yourapp.herokuapp.com/collection/products').then(
-function (response) {
-response.json().then(
-function (json) {
-// note that we used 'store.product' instead of 'this.product'
-store.products = json;
-});
-})
-}
-})
-
-const newProduct = {title: 'new product'
-, price: 49};
-// set the url to your server and route
-fetch('https://cst3145.herokuapp.com/collection/products'
-, {
-method: 'POST'
-, // set the HTTP method as 'POST'
-headers: {
-'Content-Type': 'application/json'
-, // set the data type as JSON
-},
-body: JSON.stringify(newProduct), // need to stringify the JSON object
-})
-.then(response => response.json())
-.then(responseJSON => {
-console.log('Success:'
-, responseJSON);
-});
-
-app.use(function(req, res, next) {
-// allow different IP address
-res.header("Access-Control-Allow-Origin"
-,
-"*");
-// allow different header fields
-res.header("Access-Control-Allow-Headers"
-,
-"*");
-next();
-});
-
-
 const MongoClient = require('mongodb').MongoClient;
 let db;
 MongoClient.connect('mongodb+srv://danvella02:Rapmap02@projects.qeyey.mongodb.net/Coursework2?retryWrites=true&w=majority',
